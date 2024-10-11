a0f38846e04e3f3e985c32961070a60fd4de6a81
Md Mehedi Hasan <131009381+mmhasan012@users.noreply.github.com>
2024-10-11 23:02:36 +0530

    Add files via upload

---
+++ wrapperClass.java
@@ -0,0 +1,42 @@
+/*
+In Java, a wrapper class is an object representation of a primitive data type.
+ Java provides wrapper classes for each of the eight primitive 
+ data types, allowing them to be used in situations where objects are required, 
+ such as in collections or for synchronization. The wrapper classes in Java are:
+
+byte -> Byte
+short -> Short
+int -> Integer
+long -> Long
+float -> Float
+double -> Double
+char -> Character
+boolean -> Boole
+Key Features of Wrapper Classes:
+Immutability: Wrapper class objects are immutable,
+ meaning once they are created, their values cannot be changed.
+
+Utility Methods: Wrapper classes provide a number of utility methods.
+ For example, Integer.parseInt() converts a string to an integer.
+
+Object Methods: Since wrapper classes are objects,
+ they can be used to invoke methods defined in the Object class,
+  such as toString(), equals(), and hashCode()
+
+  Autoboxing and Unboxing:
+
+Autoboxing: The automatic conversion of a primitive type to its corresponding wrapper class. For example, converting an int to an Integer.
+java
+Copy code
+int num = 5;
+Integer numObj = num; // Autoboxing
+Unboxing: The automatic conversion of a wrapper class back to its corresponding primitive type. For example, converting an Integer to an int.
+java
+Copy code
+Integer numObj = 5;
+int num = numObj; // Unboxing
+ */
+
+public class wrapperClass {
+    
+}
