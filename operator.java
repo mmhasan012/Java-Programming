901cd089c15e47935c3081cebc9e09ba0974c2bc
Md Mehedi Hasan <131009381+mmhasan012@users.noreply.github.com>
2024-10-12 23:46:13 +0530

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,14 @@
+public class operator {
+    public static void main(String[] args) {
+        
+    
+    int a = 10;
+int b = 5;
+System.out.println("a + b = " + (a + b)); // 15
+System.out.println("a - b = " + (a - b)); // 5
+System.out.println("a * b = " + (a * b)); // 50
+System.out.println("a / b = " + (a / b)); // 2
+System.out.println("a % b = " + (a % b)); // 0
+
+}
+}
