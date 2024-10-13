e0d3d44478fcc4d58fc4ea0e30a5a690badf099c
Md Mehedi Hasan <131009381+mmhasan012@users.noreply.github.com>
2024-10-13 21:13:14 +0530

    operator.java

--- operator.java
+++ operator.java
@@ -96,5 +96,62 @@ System.out.println("x || y: " + (x || y)); // true
 System.out.println("!x: " + !x);           // false
 
 
+/*
+5. Bitwise Operators
+Bitwise operators perform operations on individual bits of integer types.
+
+& Bitwise AND
+| Bitwise OR
+^ Bitwise XOR
+~ Bitwise Complement
+<< Left Shift
+>> Right Shift
+>>> Unsigned Right Shift
+
+*/
+//Example
+
+int a = 5;  // 0101 in binary
+int b = 3;  // 0011 in binary
+System.out.println("a & b: " + (a & b)); // 1  (0001 in binary)
+System.out.println("a | b: " + (a | b)); // 7  (0111 in binary)
+System.out.println("a ^ b: " + (a ^ b)); // 6  (0110 in binary)
+System.out.println("~a: " + ~a);         // -6 (inverts all bits)
+System.out.println("a << 1: " + (a << 1)); // 10 (1010 in binary)
+System.out.println("a >> 1: " + (a >> 1)); // 2  (0010 in binary)
+System.out.println("a >>> 1: " + (a >>> 1)); // 2 (unsigned right shift)
+
+
+
+/*
+6. Assignment Operators
+Assignment operators are used to assign values to variables.
+
+= Simple assignment
++= Add and assign
+-= Subtract and assign
+*= Multiply and assign
+/= Divide and assign
+%= Modulus and assign
+&= Bitwise AND and assign
+|= Bitwise OR and assign
+^= Bitwise XOR and assign
+<<= Left shift and assign
+>>= Right shift and assign
+>>>= Unsigned right shift and assign
+*/
+//Example
+int a = 10;
+a += 5;  // a = a + 5
+System.out.println("a += 5: " + a); // 15
+a -= 3;  // a = a - 3
+System.out.println("a -= 3: " + a); // 12
+a *= 2;  // a = a * 2
+System.out.println("a *= 2: " + a); // 24
+a /= 4;  // a = a / 4
+System.out.println("a /= 4: " + a); // 6
+a %= 3;  // a = a % 3
+System.out.println("a %= 3: " + a); // 0
+
 
 
