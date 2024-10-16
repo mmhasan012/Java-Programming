797c981ebfbdadafc21cba546f242767d46f6ff3
Md Mehedi Hasan <131009381+mmhasan012@users.noreply.github.com>
2024-10-16 23:32:10 +0530

    excercise.java

--- excercise.java
+++ excercise.java
@@ -133,3 +133,74 @@ class Main {
 }
 
 
+/*
+                //WonderWorks Magic Show
+The Magic Castle, the home of the Academy of Magical Arts at California has organized the great ‘WonderWorks Magic Show’.
+3 renowned magicians were invited to mystify and thrill the crowd with their world’s spectacular magic tricks.
+At the end of each of the 3 magicians’ shows, the audience were requested to give their feedback in a scale of 1 to 10.
+Number of people who watched each show and the average feedback rating of each show is known.
+Write a program to find the average feedback rating of the WonderWorks Magic show.
+
+Input Format:
+First line of the input is an integer value that corresponds to the number of people who watched show 1.
+Second line of the input is a float value that corresponds to the average rating of show 1.
+Third line of the input is an integer value that corresponds to the number of people who watched show 2.
+Fourth line of the input is a float value that corresponds to the average rating of show 2.
+Fifth line of the input is an integer value that corresponds to the number of people who watched show 3.
+Sixth line of the input is a float value that corresponds to the average rating of show 3.
+
+Output Format:
+Output should display the overall average rating for the show. Display the rating correct to 2 decimal places.
+Refer sample input and output for formatting specifications.
+[All text in bold corresponds to input and rest corresponds to output.]
+
+Sample Input and Output:
+Enter the number of people who watched show 1
+400
+Enter the average rating for show 1
+9.8
+Enter the number of people who watched show 2
+500
+Enter the average rating for show 2
+9.6
+Enter the number of people who watched show 3
+100
+Enter the average rating for show 3
+5
+The overall average rating for the show is 9.22
+*/
+//code:
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        
+            Scanner sc =new Scanner(System.in);
+
+            System.out.println("Enter the number of people who watched show 1");
+            int s1=sc.nextInt();
+            System.out.println("Enter the average rating for show 1");
+            float r1=sc.nextFloat();
+
+            System.out.println("Enter the number of people who watched show 2");
+            int s2=sc.nextInt();
+            System.out.println("Enter the average rating for show 2");
+            float r2=sc.nextFloat();
+
+
+            System.out.println("Enter the number of people who watched show 3");
+            int s3=sc.nextInt();
+            System.out.println("Enter the average rating for show 3");
+            float r3=sc.nextFloat();
+
+            int totalPeople=s1+s2+s3;
+
+            float totalRatingInShow1=s1*r1;
+            float totalRatingInShow2=s2*r2;
+            float totalRatingInShow3=s3*r3;
+
+            float overAllAvgRating=(totalRatingInShow1+totalRatingInShow2+totalRatingInShow3)/totalPeople;
+            System.out.printf("The overall average rating for the show is %.2f",overAllAvgRating);
+
+        
+     }
+}
