d0783aeda92cd18a653144fddf081edaa159fc3d
Md Mehedi Hasan <131009381+mmhasan012@users.noreply.github.com>
2024-10-17 23:09:34 +0530

    excercise.java

--- excercise.java
+++ excercise.java
@@ -254,3 +254,52 @@ class Main {
         
      }
 }
+
+
+/*Problem
+
+Candy Game
+Mona set off with great zeal to the "Fun Fair 2017". There were numerous activities in the fair, though Mona liked the Candy game. Delicious candies were wrapped in colourful foiled sheets with some random numbers on each of the candies. The game coordinators then formed many groups of few candies together, such that each candy group makes an integer and hid them all around the room. The objective of the game is that the players should look for the occurrences of number four anywhere in the integers (candy groups) placed in the room.
+
+Mona started off with the game where there are many such integers, for each of them she should calculate the number of occurrences of the digit 4 in the decimal representation. Can you please help her in succeeding the game?
+
+Input Format:
+The only line of input contains a single integer from the candy group.
+
+Output Format:
+Output should contain the number of occurrences of the digit 4 in the respective integer from the candy groups that Mona gets.
+Refer sample input and output for formatting specifications.
+
+Sample Input 1:
+447474
+
+Sample Output 1:
+4
+
+Sample Input 2:
+12
+
+Sample Output 2:
+0
+*/
+
+//code:
+import java.util.*;
+
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        String number =sc.nextLine();
+
+        int count=0;
+
+        for( char digit : number.toCharArray() ){
+            if(digit=='4'){
+                count++;
+            }
+        }
+        System.out.printf("%d",count);
+    }
+}
