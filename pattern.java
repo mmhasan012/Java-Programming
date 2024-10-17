7904c8717e0daa344d38595e1eaa91ac1971ccf5
Md Mehedi Hasan <131009381+mmhasan012@users.noreply.github.com>
2024-10-17 14:42:21 +0530

    pattern.java

--- pattern.java
+++ pattern.java
@@ -29,3 +29,31 @@ class main{
         
     }
   
+/*
+Enter the line number
+4
+A
+AB
+ABC
+ABCD
+
+*/
+
+
+//code:
+import java.util.*;
+class Main{
+    public static void main(String[]s){
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter the line number");
+        int n=sc.nextInt();
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=row;col++){
+                System.out.printf("%c",col+64);
+            }
+            System.out.println();
+        }
+    }
+}
+
