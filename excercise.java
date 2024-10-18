de6eea173b7ec48ee50e488aa381054be6cb64fd
Md Mehedi Hasan <131009381+mmhasan012@users.noreply.github.com>
2024-10-18 22:35:45 +0530

    excercise.java

--- excercise.java
+++ excercise.java
@@ -403,3 +403,58 @@ import java.util.*;
 
        }
    }
+/*
+SPECIAL NUMBER
+
+A 2-digit number is said to be a special number if the sum of the sum of its digits and the products of its digits is equal to the number itself.
+
+For example, 19 is a special number. The digits in 19 are 1 and 9. The sum of the digits is 10 and the product of the digits is 9.
+
+10+9 = 19.
+
+Write a program to find all special numbers between 2 limits m and n(both inclusive). Assume that m and n are 2-digit numbers.
+
+Input Format:
+
+Input consists of 2 integers m and n.
+
+Output Format:
+
+Refer Sample Output .
+
+Sample Input 1:
+
+11
+
+30
+
+Sample Output 1:
+
+19
+
+29
+*/
+//code:
+
+ import java.util.*;
+   class Main{
+       public static void main(String[]s){
+           Scanner sc=new Scanner(System.in);
+           int m=sc.nextInt();
+           int n=sc.nextInt();
+
+           //int m,n;
+           while(m<n){
+               int tenTh=m%10;
+               int unit=m/10;
+               int sum1=tenTh+unit;
+               int product=tenTh*unit;
+               int sum2=sum1+product;
+
+               if(m==sum2){
+                   System.out.println(+m);
+               }
+               m++;
+           }
+       }
+   }
