2ada27848501a8f79b1c09a741d921cb591eb7ca
Md Mehedi Hasan <131009381+mmhasan012@users.noreply.github.com>
2024-10-21 22:56:47 +0530

    Update ExerciseDatatypeWrapperClass.java

--- ExerciseDatatypeWrapperClass.java
+++ ExerciseDatatypeWrapperClass.java
@@ -79,3 +79,41 @@ Dinesh's house is located at (5.7,9.0)
            
        }
    }
+
+
+
+/*
+Nikhil, the founder of “Pine Tree” company wished to design an Event Management System that would let its Customers plan and host events seamlessly via an online platform.
+
+As a part of this requirement, Nikhil wanted to write a piece of code for his company’s Amphi Event Management System that will display customized welcome messages by taking Customers’ name as input. Help Nikhil on the task.
+
+Input Format:
+First line of the input is a string that corresponds to a Customer’s name. Assume that the maximum length of the string is 50.
+
+Output Format:
+Output should display the welcome message along with the Customer’s name.
+Refer sample input and output for formatting specifications.
+[All text in bold corresponds to input and rest corresponds to output.]
+
+Sample Input and Output:
+Enter your name
+Beena
+Hello Beena ! Welcome to Amphi Event Management System
+*/
+//code:
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        
+            // Fill your code here
+            
+
+            Scanner sc =new Scanner (System.in);
+            System.out.println("Enter your name");
+            String name = sc.nextLine();
+            System.out.println("Hello " + name + " ! Welcome to Amphi Event Management System");
+
+        
+     }
+}
