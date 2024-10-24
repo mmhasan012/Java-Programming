e73e8270beb264978e2b8e634df286b174094650
Md Mehedi Hasan <131009381+mmhasan012@users.noreply.github.com>
2024-10-24 23:59:30 +0530

    Update 5.2 operator Exercise.java

--- 5.2 operator Exercise.java
+++ 5.2 operator Exercise.java
@@ -1,1 +1,45 @@
+/*
+People on board
 
+Pan Am 73 was ready to take off. But before the take off Neerja wanted to take a count of the total number of people inside the flight.
+
+
+
+
+The cockpit crew consisted of the Pilot In Command, the Co-Pilot and the Radio Engineer. The cabin crew consisted of 15 crew members including Neerja.
+
+
+
+Given the number of men, women and children on board find the total number of people on the aircraft.
+Input Format:
+The input consists of three integers M, W and C corresponding to the number of men, women and children.
+
+Output Format:
+The output should display an integer corresponding to the total number of people in the aircraft, in a single line.
+
+Sample Input:
+90
+100
+85
+Sample Output:
+293
+
+*/
+//code:
+   import java.util.*;
+   class Main{
+       public static void main(String[]s){
+
+
+           Scanner sc=new Scanner(System.in);
+           int m=sc.nextInt();
+            int w=sc.nextInt();
+            int c=sc.nextInt();
+
+            int sum=m+w+c+18;
+            System.out.printf("%d",sum);
+
+
+
+       }
+   }
