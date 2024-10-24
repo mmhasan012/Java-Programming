aac7438e5ba41ee56c37d5a7e40384ff4438f580
Md Mehedi Hasan <131009381+mmhasan012@users.noreply.github.com>
2024-10-25 00:03:16 +0530

    Update 5.2 operator Exercise.java

--- 5.2 operator Exercise.java
+++ 5.2 operator Exercise.java
@@ -43,29 +43,28 @@ Sample Output:
 
        }
    }
-/*Passenger Count
-
-Pan Am 73 was ready to take off. But before the take off Neerja wanted to take a count of the total number of passengers on board. The cockpit crew consisted of the Pilot In Command, the Co-Pilot and the Radio Engineer.
-
-
-
+/*
+Passenger Count
+Pan Am 73 was ready to take off. But before the take off Neerja wanted to take a count of the total number of passengers on board. 
+The cockpit crew consisted of the Pilot In Command, the Co-Pilot and the Radio Engineer.
 The cabin crew consisted of 15 crew members including Neerja. Given the total number of people on the aircraft find the number of passengers.
-
-
-
 Input Format:
-
 The input consists of one integer T corresponding to the total number of people in the aircraft.
 
 Output Format:
-
 The output should display an integer corresponding to the total number of passengers in the aircraft, in a single line.
-
 Sample Input:
-
 293
-
 Sample Output:
-
 275
 */
+//code:
+   import java.util.*;
+   class Main{
+       public static void main(String[]s){
+           Scanner sc=new Scanner(System.in);
+           int total=sc.nextInt();
+           int p=total-18;
+           System.out.printf("%d",p);
+       }
+   }
