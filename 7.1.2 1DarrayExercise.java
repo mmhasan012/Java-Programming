22ba50da9a403dff73d58cbc46d9699a1c825436
Md Mehedi Hasan <131009381+mmhasan012@users.noreply.github.com>
2024-11-05 22:15:19 +0530

    Update 7.1.2 1DarrayExercise.java

--- 7.1.2 1DarrayExercise.java
+++ 7.1.2 1DarrayExercise.java
@@ -132,3 +132,37 @@ class Main{
 
     }
 }
+
+/* 
+
+Cumulative sum
+
+Write a program to form a cumulative sum array from an array.
+
+For Eg, for the array {2,3,5,7,1}, the cumulative sum array is {2,5,10,17,18}
+
+Note:
+Refer to the problem requirements.
+
+Input Format:
+Input consists of n+1 integers. The first integer corresponds to ‘n’, the size of the array.
+The next ‘n’ integers correspond to the elements in the array. Assume that the maximum value of n is 15.
+Output Format:
+Refer sample output for details. The integers in the output are separated by a single space.
+[All text in bold corresponds to input and the rest corresponds to output]
+
+Sample Input 1:
+Enter the number of elements in an array
+5
+Enter the array elements
+2
+3
+5
+7
+1
+
+Sample Output 1:
+Cumulative sum array
+2 5 10 17 18
+
+*/
