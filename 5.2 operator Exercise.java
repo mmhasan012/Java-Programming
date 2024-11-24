9d278ceeb1439df605f73192dcf26ca98957e2e0
Md Mehedi Hasan <131009381+mmhasan012@users.noreply.github.com>
2024-11-24 10:04:08 +0530

    Update 5.2 operator Exercise.java

--- 5.2 operator Exercise.java
+++ 5.2 operator Exercise.java
@@ -528,31 +528,42 @@ class Main {
      }
 }
 /*
-Wisconsin State Fair
-Wisconsin State Fair is one of the largest midsummer celebrations in the Midwest Allis, showcasing the agriculture skills and prowess of the state.
-The Event organizers hired few part-time employees to work at the fair and the agreed salary paid to them are as given below:
+Birthday Challenge
+Louis was celebrating his 10th Birthday and his parents wished to make his birthday more special by 
+throwing a surprise bash, inviting all their friends, relatives and neighbors. The little mathematics geek
+Louis has another surprise waiting for him when he had to cut his favorite Choco vanilla cake. The cake is
+a rectangular cake and it consists of m×n (1≤m, n≤1000) squares. His friends now called him out for a challenge.
 
-Weekdays --- 80 / hour
-Weekends --- 50 / hour
+The challenge is that, Louis has to break the cake up into 1×1 pieces (individual squares) and find
+what is the minimum number of times that he breaks the choco vanilla cake, or pieces therefore, in order to achieve this?
 
-Justin is a part-time employee working at the fair. Number of hours Justin has worked in the weekdays is 10 more than the number
-of hours he had worked during weekends. If the total salary paid to him in this month is known, write a program to estimate 
-the number of hours he had worked during weekdays and the number of hours he had worked during weekends.
+Note that he cannot stack pieces of the cake and break them, because the choco vanilla cake is thick.
+As an example, a 2×2 cake requires 3 breaks. First he can break it in half, then break each of the halves in half.
+He cannot break it in half, stack the two 1×2 pieces, and then use only one more break to achieve his goal.
 
 Input Format:
-First line of the input is a double value that corresponds to the total salary paid to Justin.
+First line of the input consists of an integer, the dimensions m of the choco vanilla cake.
+Second line of the input consists of an integer, the dimensions n of the choco vanilla cake.
 
 Output Format:
-First line of the output should display the number of hours Justin has worked during the weekdays.
-Second line of the output should display the number of hours Justin has worked during the weekends.
+Output the minimum number of times that he breaks the choco vanilla cake
 Refer sample input and output for formatting specifications.
 [All text in bold corresponds to input and rest corresponds to output.]
 
-Sample Input and Output:
-Enter the total salary paid
-2750
-Number of weekday hours is 25
-Number of weekend hours is 15
+Sample Input and Output 1:
+Enter m
+1
+Enter n
+2
+Minimum number of times is 1
+
+Sample Input and Output 2:
+Enter m
+2
+Enter n
+2
+Minimum number of times is 3
 
 */
 //code:
+
